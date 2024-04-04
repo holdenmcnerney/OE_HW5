6c7a3896136ad4d72fa1a622791bf4a0512bc490
Holden McNerney <holden.mcnerney@maxar.com>
2024-04-04 16:08:23 -0600

    continued work on EKF
    I think the linearized dynamics jacobian is wrong

--- main.py
+++ main.py
@@ -17,14 +17,11 @@ def F_km1_build(h_km1, s_km1, Cb_km1):
     '''
     Temp
     '''
-    h_km1 = h_km1[0]
-    s_km1 = s_km1[0]
-    Cb_km1 = Cb_km1[0]
     F_km1 = np.array([[1, dt, 0], 
                       [dt * (- rho0 * s_km1**2 / (2 * Cb_km1 * hp) * np.exp(-h_km1 / hp)
                              + 2 * g0 * (RE**2 / (RE + h_km1)**3)), 
                        1 + dt * rho0 * s_km1 / Cb_km1 * np.exp(-h_km1 / hp), 
-                       dt * rho0 * s_km1**2 / (2 * Cb_km1**2) * np.exp(h_km1 / hp)], 
+                       dt * rho0 * s_km1**2 / (2 * Cb_km1**2) * np.exp(-h_km1 / hp)], 
                       [0, 0, 1]])
     return F_km1
 
@@ -32,7 +29,6 @@ def H_km1_build(h_km1):
     '''
     Temp
     '''
-    h_km1 = h_km1[0][0]
     H_km1 = np.array([h_km1 * (d**2 + h_km1**2)**(-1/2), 0, 0])
     return H_km1
 
@@ -49,18 +45,24 @@ def EKF(y, x0, P0, Q, R):
             x_hist[0, :] = np.atleast_2d(x0).T
         else:
             # Prediction Step
-            x_k_km1 = f_km1_build(x_km1_km1[0], x_km1_km1[1], x_km1_km1[2])
-            F_km1 = F_km1_build(x_km1_km1[0], x_km1_km1[1], x_km1_km1[2])
+            h_km1 = x_km1_km1[0][0]
+            s_km1 = x_km1_km1[1][0]
+            Cb_km1 = x_km1_km1[2][0]
+            x_k_km1 = f_km1_build(h_km1, s_km1, Cb_km1)
+            F_km1 = F_km1_build(h_km1, s_km1, Cb_km1)
             P_k_km1 = F_km1 @ P_km1_km1 @ F_km1.T + Q
-            y_k_est = np.sqrt(d**2 + x_k_km1[0]**2) 
+            h_k_km1 = x_k_km1[0][0]
+            y_k_est = np.sqrt(d**2 + h_k_km1**2) 
 
             # Correction Step
             y_diff = y_k - y_k_est
-            H_km1 = H_km1_build(x_k_km1[0])
+            H_km1 = np.atleast_2d(H_km1_build(h_km1))
             S_k = H_km1 @ P_k_km1 @ H_km1.T + R
             K_k = P_k_km1 @ H_km1.T * S_k**-1
             x_k_k = x_k_km1 + K_k * y_diff
-            P_k_k = P_k_km1 - K_k @ S_k * K_k.T
+            A = np.eye(3) - K_k @ H_km1
+            P_k_k = A @ P_k_km1 @ A.T + K_k * R @ K_k.T
+            # P_k_k = P_k_km1 - K_k @ S_k * K_k.T
 
             # Saving and reseting values
             x_hist[i, :] = np.atleast_2d(x_k_k).T
@@ -76,10 +78,10 @@ def main():
     # Load Data
     data = np.loadtxt('altimeter_data.csv', delimiter=',')
     time = data[:, 0]
-    y_k = data[:, 0]                    # Current range measurement
-    h_k = data[:, 0]                    # True altitude
-    s_k = data[:, 0]                    # True vertical velocity
-    C_bk = data[:, 0]                   # True ballistic coefficient
+    y_k = data[:, 1]                    # Current range measurement
+    h_k = data[:, 2]                    # True altitude
+    s_k = data[:, 3]                    # True vertical velocity
+    C_bk = data[:, 4]                   # True ballistic coefficient
     # Constants Definition
     global dt, rho0, g0, hp, RE, d
     dt = 0.5                                    # s
@@ -95,6 +97,15 @@ def main():
     P0 = np.diagflat((100**2, 10**2, 1**2))     # [ft^2, ft^2/s^2, lb^2/ft^2]
 
     EKF_x_hist = EKF(y_k, x0, P0, Q, R)
+
+    plt.plot(time, EKF_x_hist[:, 0])
+    plt.plot(time, h_k)
+    ax = plt.gca()
+    ax.set_xlim([-5, time[-1]])
+    ax.set_ylim([0, 410000])
+    # ax.set_ylim([-6000, 0])
+    plt.show()
+
     return 0
 
 if __name__=='__main__':
