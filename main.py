c0d57daaa8b579f8002ee019e323e40df183d6c3
Holden McNerney <holden.mcnerney@maxar.com>
2024-04-04 16:24:24 -0600

    seemed to fix EKF but Cb is off

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def F_km1_build(h_km1, s_km1, Cb_km1):
                       [dt * (- rho0 * s_km1**2 / (2 * Cb_km1 * hp) * np.exp(-h_km1 / hp)
                              + 2 * g0 * (RE**2 / (RE + h_km1)**3)), 
                        1 + dt * rho0 * s_km1 / Cb_km1 * np.exp(-h_km1 / hp), 
-                       dt * rho0 * s_km1**2 / (2 * Cb_km1**2) * np.exp(-h_km1 / hp)], 
+                       - dt * rho0 * s_km1**2 / (2 * Cb_km1**2) * np.exp(-h_km1 / hp)], 
                       [0, 0, 1]])
     return F_km1
 
@@ -60,9 +60,9 @@ def EKF(y, x0, P0, Q, R):
             S_k = H_km1 @ P_k_km1 @ H_km1.T + R
             K_k = P_k_km1 @ H_km1.T * S_k**-1
             x_k_k = x_k_km1 + K_k * y_diff
-            A = np.eye(3) - K_k @ H_km1
-            P_k_k = A @ P_k_km1 @ A.T + K_k * R @ K_k.T
-            # P_k_k = P_k_km1 - K_k @ S_k * K_k.T
+            # A = np.eye(3) - K_k @ H_km1
+            # P_k_k = A @ P_k_km1 @ A.T + K_k * R @ K_k.T
+            P_k_k = P_k_km1 - K_k @ S_k * K_k.T
 
             # Saving and reseting values
             x_hist[i, :] = np.atleast_2d(x_k_k).T
@@ -70,6 +70,23 @@ def EKF(y, x0, P0, Q, R):
             P_km1_km1 = P_k_k
 
     return x_hist
+
+def make_pretty_plot(time, EKF_x_hist, h_k, s_k, Cb_k):
+    '''
+    Temp
+    '''
+    fig, ax = plt.subplots((3))
+    ax[0].plot(time, EKF_x_hist[:, 0], color='g', label='Estimate')
+    ax[0].plot(time, h_k, color='y', label='True Value')
+    ax[1].plot(time, EKF_x_hist[:, 1], color='g', label='Estimate')
+    ax[1].plot(time, s_k, color='y', label='True Value')
+    ax[2].plot(time, EKF_x_hist[:, 2], color='g', label='Estimate')
+    ax[2].plot(time, Cb_k, color='y', label='True Value')
+    ax[0].legend()
+    ax[1].legend()
+    ax[2].legend()
+    plt.show()
+    return 0
 
 def main():
     '''
@@ -81,7 +98,7 @@ def main():
     y_k = data[:, 1]                    # Current range measurement
     h_k = data[:, 2]                    # True altitude
     s_k = data[:, 3]                    # True vertical velocity
-    C_bk = data[:, 4]                   # True ballistic coefficient
+    Cb_k = data[:, 4]                   # True ballistic coefficient
     # Constants Definition
     global dt, rho0, g0, hp, RE, d
     dt = 0.5                                    # s
@@ -98,13 +115,7 @@ def main():
 
     EKF_x_hist = EKF(y_k, x0, P0, Q, R)
 
-    plt.plot(time, EKF_x_hist[:, 0])
-    plt.plot(time, h_k)
-    ax = plt.gca()
-    ax.set_xlim([-5, time[-1]])
-    ax.set_ylim([0, 410000])
-    # ax.set_ylim([-6000, 0])
-    plt.show()
+    make_pretty_plot(time, EKF_x_hist, h_k, s_k, Cb_k)
 
     return 0
 
